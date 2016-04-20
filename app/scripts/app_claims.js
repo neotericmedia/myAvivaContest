d8c49f30b77cf8af799f4319792ab6250722fbe4
Carlos Arturo Ramirez <eramirezcarlos@Carloss-MacBook-Pro.local>
2016-04-20 13:48:01 -0400

    initial javascript configuration

---
+++ app/scripts/app_claims.js
@@ -0,0 +1,21 @@
+var claims_app = {
+
+  common: {
+    init: function () {
+
+
+    }
+  },
+  'overview-page': {
+    init: function () {
+      // Availability validations initialization
+
+    },
+    settings: function () {
+      // Availability settings
+    },
+    render: function () {
+      // render
+    }
+  },
+}
---
+++ app/scripts/app_claims_plugins.js
@@ -0,0 +1,15 @@
+claims_app.plugins = {
+  // your webservices has an init as well
+  init: function () {
+    // Initialization of the code
+    // console.log(' gi_app.plugins: init called');
+
+  },
+  update: function () {
+    // code to execute the function update as an example
+
+  },
+
+}
+
+
